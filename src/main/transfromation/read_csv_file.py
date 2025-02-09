cd76a3071d319be3603c6a9de230ef561134897b
sunny nuri <Sunnynuri.pc@gmail.com>
2025-02-09 11:49:48 +0530

    read file

---
+++ src/main/transfromation/read_csv_file.py
@@ -0,0 +1,24 @@
+from enum import nonmember
+
+from src.main.utility.spark_session import spark_session
+from loguru import logger
+
+def read_csv(spark,file_path):
+
+    try:
+        #read the csv file into a DataFrame
+        logger.info(f"------Reading CSV file from path {file_path}------")
+        df=spark.read.csv(file_path,header=True, inferSchema=True)
+
+        logger.info("------CSV file read successfully------")
+        df.show()
+        input()
+
+    except Exception as e:
+        logger.error(f"Error reading the csv file:{e}")
+        return None
+
+spark=spark_session()
+csv_file_path="E:\\spark_project01\\files\\raw_files\\sample_data.csv"
+
+read_csv(spark,csv_file_path)
