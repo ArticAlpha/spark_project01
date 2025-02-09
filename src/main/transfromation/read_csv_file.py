61e69df1cbf77d15b2c57b0a53ede7ad21b6cf6d
sunny nuri <Sunnynuri.pc@gmail.com>
2025-02-09 18:41:46 +0530

    read data

--- src/main/transfromation/read_csv_file.py
+++ src/main/transfromation/read_csv_file.py
@@ -8,11 +8,16 @@ def read_csv(spark,file_path):
     try:
         #read the csv file into a DataFrame
         logger.info(f"------Reading CSV file from path {file_path}------")
-        df=spark.read.csv(file_path,header=True, inferSchema=True)
+        df=spark.read.format("csv")\
+                .option("header","true")\
+                .option("infer_schema","true")\
+                .option("multiline","true")\
+                .option("escape","\"")\
+                .load(file_path)
 
         logger.info("------CSV file read successfully------")
-        df.show()
-        input()
+        df.select("order_id","customer_gender").show()
+        # input()
 
     except Exception as e:
         logger.error(f"Error reading the csv file:{e}")
@@ -20,5 +25,4 @@ def read_csv(spark,file_path):
 
 spark=spark_session()
 csv_file_path="E:\\spark_project01\\src\\testing\\car_sales.csv"
-
 read_csv(spark,csv_file_path)
