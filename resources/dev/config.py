ed93ea35ad8802a5dd038f896c50c54e4628b942
sunny nuri <Sunnynuri.pc@gmail.com>
2025-03-16 23:55:50 +0530

    refactor code and add key generation scripts

--- resources/dev/config.py
+++ resources/dev/config.py
@@ -4,7 +4,7 @@ mysql_password="Password"
 mysql_database = "sunny"
 
 # data cleaning file location
-csv_file_path = "E:/spark_project01/src/testing/car_sales.csv"
+csv_file_path = "E:/spark_project01/files/raw_files/sample_data.csv" #"E:/spark_project01/src/testing/car_sales.csv"
 invalid_price_path = "E:/spark_project01/files/invalid_data/invalid_data.csv"
 cleaned_data_path = "E:/spark_project01/files/cleaned_data/parquet" #will be used in data_transformation.py
 
